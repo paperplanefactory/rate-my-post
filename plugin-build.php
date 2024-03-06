8202e9aac5a007795a598282b682245e71b13869
Collins Agbonghama <collizo4sky@gmail.com>
2024-03-06 18:03:01 +0100

    wip

--- plugin-build.php
+++ plugin-build.php
@@ -1,4 +1,5 @@
 <?php
+
 $dir               = @$argv[1] ?: dirname(__FILE__) . '/build';
 $plugin_git_name   = 'rate-my-post';
 $plugin_git_folder = $dir . '/' . $plugin_git_name;
@@ -24,13 +25,16 @@ exec("git clone https://github.com/feedbackwp/rate-my-post.git $plugin_git_name"
 echo "git clone completed." . "\n";
 echo "changed directory to $plugin_git_folder" . "\n";
 $chdir = chdir($plugin_git_folder);
-if ( ! $chdir) exit;
+if ( ! $chdir) {
+    exit;
+}
 
 deleteDir('.git');
 deleteDir('_dev');
 deleteDir('build');
 deleteDir('tests');
 deleteDir('codekit');
+@unlink('.gitignore');
 
 echo ".git and tests folders deleted" . "\n";
 
@@ -57,7 +61,7 @@ foreach (
 }
 
 //exec("makepot ../" . $plugin_git_name);
-exec("wp i18n make-pot ../" . $plugin_git_name." languages/rate-my-post.pot");
+exec("wp i18n make-pot ../" . $plugin_git_name . " languages/rate-my-post.pot");
 
 // move up directory
 chdir($dir);
