34a8a631146164f6db92513e3b157c83596d7509
Collins Agbonghama <collizo4sky@gmail.com>
2024-09-06 11:29:13 +0100

    Added filter rmp_is_show_for_post_edit_screen

--- common/class-rate-my-post-common.php
+++ common/class-rate-my-post-common.php
@@ -216,9 +216,12 @@ class Rate_My_Post_Common
     {
         global $post;
 
-        if ( ! isset($post->post_type)) return false;
+        if (isset($post->post_type)) {
 
-        if (in_array($post->post_type, self::enabled_post_types())) return true;
+            $is_post_type_check_success = in_array($post->post_type, self::enabled_post_types());
+
+            if (apply_filters('rmp_is_show_for_post_edit_screen', $is_post_type_check_success, $post)) return true;
+        }
 
         return false;
     }
